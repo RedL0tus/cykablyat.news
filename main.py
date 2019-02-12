45432abd06bccf7e47f59f4db2ecfae66625542b
Kay Lin <kaymw@aosc.io>
2019-02-11 17:00:32 -0800

    Switch to sanic
    Signed-off-by: Kay Lin <kaymw@aosc.io>

--- main.py
+++ main.py
@@ -2,31 +2,28 @@
 # -*- encoding: utf-8 -*-
 
 import json
-import jinja2
 import random
-import aiohttp_jinja2
 
-from aiohttp import web
+from sanic import Sanic
+from sanic_jinja2 import SanicJinja2
 
 CONF = 'config.json'
 
-routes = web.RouteTableDef()
+app = Sanic(__name__)
 
-news = []
+jinja = SanicJinja2(app, pkg_path='.')
+
+news = {}
 with open(CONF) as f:
     news = json.loads(f.read())
 
 
-@routes.get('/')
-@aiohttp_jinja2.template('template.html')
+@app.route('/')
+@jinja.template('template.html')
 async def redir(request):
     passage = random.choice(news['news'])
     return {'news': passage}
 
+
 if __name__ == '__main__':
-    app = web.Application()
-    aiohttp_jinja2.setup(
-        app, loader=jinja2.FileSystemLoader('.')
-    )
-    app.add_routes(routes)
-    web.run_app(app, port=10010)
+    app.run(host='0.0.0.0', port=10010)
