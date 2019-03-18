de91cf51e087015cc7ddfb6f36e5b235b65eec00
Kay Lin <kaymw@aosc.io>
2019-03-17 20:50:07 -0700

    Check commit hash
    Signed-off-by: Kay Lin <kaymw@aosc.io>

--- on_push.sh
+++ on_push.sh
@@ -6,6 +6,9 @@ PID_FILE="cykablyat.pid";
 SYSTEMCTL="systemctl";
 SYSTEMD_UNIT="cykablyat.service";
 
+# Get the current commit
+CURRENT_COMMIT=$(git log --pretty=format:'%H' -n 1);
+
 git pull;
 
 # Check PID file
@@ -15,7 +18,13 @@ if [[ ! -f "${PID_FILE}" ]]; then
 fi
 
 # Check changed files
-if [[ $(git diff --name-only HEAD~1 HEAD~2) == *".py"* ]]; then
+NEW_COMMIT=$(git log --pretty=format:'%H' -n 1);
+if [[ ${CURRENT_COMMIT} == ${NEW_COMMIT} ]]; then
+	echo "No new commit found, quitting...";
+	exit 0;
+fi
+
+if [[ $(git diff --name-only ${CURRENT_COMMIT} ${NEW_COMMIT}) == *".py"* ]]; then
 	echo "Python file changed, restarting service";
 	sudo ${SYSTEMCTL} restart ${SYSTEMD_UNIT};
 else
