d95d6fdff2cf9bce5d680f972d5c6636a17cefdf
Kay Lin <kaymw@aosc.io>
2019-03-17 20:59:36 -0700

    Update Python dependecies on push event
    Signed-off-by: Kay Lin <kaymw@aosc.io>

--- on_push.sh
+++ on_push.sh
@@ -2,8 +2,10 @@
 
 set -e;
 
+PIP="venv/bin/pip3";
 PID_FILE="cykablyat.pid";
 SYSTEMCTL="systemctl";
+REQUIREMENTS="requirements.txt";
 SYSTEMD_UNIT="cykablyat.service";
 
 # Get the current commit
@@ -19,6 +21,7 @@ fi
 
 # Check changed files
 NEW_COMMIT=$(git log --pretty=format:'%H' -n 1);
+
 if [[ ${CURRENT_COMMIT} == ${NEW_COMMIT} ]]; then
 	echo "No new commit found, quitting...";
 	exit 0;
@@ -26,6 +29,10 @@ fi
 
 if [[ $(git diff --name-only ${CURRENT_COMMIT} ${NEW_COMMIT}) == *".py"* ]]; then
 	echo "Python file changed, restarting service";
+	# Update Python dependencies
+	if [[ -f ${REQUIREMENTS} && -f ${PIP} ]]; then
+		${PIP} install --upgrade -r ${REQUIREMENTS};
+	fi
 	sudo ${SYSTEMCTL} restart ${SYSTEMD_UNIT};
 else
 	echo "Reloading config files";
